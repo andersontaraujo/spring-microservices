613ca7d5491ddbc6e5ade8773a23f2b744e18ef9
Anderson Targino <andersontargino@cers.com.br>
2019-05-11 13:42:24 -0300

    feat(app): Ajustado conversão entre LocalDateTime/Date.

--- user-service/src/main/java/com/devaware/userservice/util/LocalDateTimeConverter.java
+++ user-service/src/main/java/com/devaware/userservice/util/LocalDateTimeConverter.java
@@ -1,22 +1,22 @@
 package com.devaware.userservice.util;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
 import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.metadata.Type;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 public class LocalDateTimeConverter extends BidirectionalConverter<Date, LocalDateTime> {
 
     @Override
     public LocalDateTime convertTo(Date source, Type<LocalDateTime> destinationType) {
-        return LocalDateTime.ofInstant(source.toInstant(), ZoneId.systemDefault());
+        return LocalDateTime.ofInstant(source.toInstant(), ZoneOffset.UTC);
     }
 
     @Override
     public Date convertFrom(LocalDateTime source, Type<Date> destinationType) {
-        return Date.from(source.atZone(ZoneId.systemDefault()).toInstant());
+        return Date.from(source.toInstant(ZoneOffset.UTC));
     }
 
 }
